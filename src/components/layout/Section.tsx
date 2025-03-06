dcf6384beba9e47e21ea22f3db572ccb9bb8f869
SarahAbigayle <62311528+SarahAbigayle@users.noreply.github.com>
2025-03-05 18:44:29 -0600

    Moving files

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -1,6 +1,4 @@
 import './Section.css';
-import StarVector from '../../images/StarVector.svg';
-import StarDivider from '../../images/StarDivider.svg';
 
 interface IProps {
     sectionTitle: string;
@@ -11,18 +9,21 @@ interface IProps {
 export const Section = (props: IProps) => {
     const {sectionTitle, sectionContent, className} = props;
 
+    const starVectorSVG = `${process.env.PUBLIC_URL}/images/StarVector.svg`;
+    const starDividerSVG = `${process.env.PUBLIC_URL}/images/StarDivider.svg`;
+
     return (
         <div className={'section-container ' + className}>
             <div className='section-title'>
-                <img src={StarVector} className='section-star' />
+                <img src={starVectorSVG} className='section-star' />
                 <h2>{sectionTitle}</h2>
-                <img src={StarVector} className='section-star' />
+                <img src={starVectorSVG} className='section-star' />
             </div>
             <div className='section'>
                 {sectionContent}
             </div>
             <div className='section-divider'>
-                <img src={StarDivider}/>
+                <img src={starDividerSVG}/>
             </div>
         </div>
     )
